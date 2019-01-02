3a154c9827b8a2012a67b392efaed473f66a392e
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2019-01-01 20:02:24 -0500

    style(Square): Give it basic styles so we can work on the board

--- client/src/Board/Square/index.js
+++ client/src/Board/Square/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid gray;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 export default function Square({rank, file}) {
-  return <p>{file + rank}</p>;
+  return <Container>{file + rank}</Container>;
 }
 
 Square.propTypes = {

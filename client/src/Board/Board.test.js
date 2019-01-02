999e752ac2dc652f6b19290da75b31db5f86155f
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2019-01-01 19:38:55 -0500

    refactor(tests): Clean up shitty repitition in file tests

--- client/src/Board/Board.test.js
+++ client/src/Board/Board.test.js
@@ -34,67 +34,42 @@ describe("Square props", () => {
   })
 
   describe('the value of the file props', () => {
-    test("A file squares are correct", () => {
-      const squares = getSquares();
+    const files = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h'
+    ];
 
-      [0, 8, 16, 24, 32, 40, 48, 56].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("a");
-      })
-    })
+    files.forEach((file, fileIndex) => {
+      test(`${file.toUpperCase()} file squares are correct`, () => {
+        const squares = getSquares();
 
-    test("B file squares are correct", () => {
-      const squares = getSquares();
+        // [0, 8, 16, 24, 32, 40, 48, 56]
+        // [1, 9, 17, 25, 33, 41, 49, 57]
+        // [2, 10, 18, 26, 34, 42, 50, 58]
+        // [3, 11, 19, 27, 35, 43, 51, 59]
+        // [4, 12, 20, 28, 36, 44, 52, 60]
+        // [5, 13, 21, 29, 37, 45, 53, 61]
+        // [6, 14, 22, 30, 38, 46, 54, 62]
+        // [7, 15, 23, 31, 39, 47, 55, 63]
+        const testIndicies = Array(8).fill('').map((_, i) => {
+          let indexOfSquareToTest = i;
 
-      [1, 9, 17, 25, 33, 41, 49, 57].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("b");
-      })
-    })
+          if (fileIndex === 0 && i === 0) {
+            return 0;
+          }
 
-    test("C file squares are correct", () => {
-      const squares = getSquares();
+          return (i * 8) + fileIndex;
+        })
 
-      [2, 10, 18, 26, 34, 42, 50, 58].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("c");
-      })
-    })
-
-    test("D file squares are correct", () => {
-      const squares = getSquares();
-
-      [3, 11, 19, 27, 35, 43, 51, 59].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("d");
-      })
-    })
-
-    test("E file squares are correct", () => {
-      const squares = getSquares();
-
-      [4, 12, 20, 28, 36, 44, 52, 60].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("e");
-      })
-    })
-
-    test("F file squares are correct", () => {
-      const squares = getSquares();
-
-      [5, 13, 21, 29, 37, 45, 53, 61].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("f");
-      })
-    })
-
-    test("G file squares are correct", () => {
-      const squares = getSquares();
-
-      [6, 14, 22, 30, 38, 46, 54, 62].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("g");
-      })
-    })
-
-    test("H file squares are correct", () => {
-      const squares = getSquares();
-
-      [7, 15, 23, 31, 39, 47, 55, 63].forEach(index => {
-        expect(squares[index].prop("file")).toEqual("h");
+        testIndicies.forEach(index => {
+          expect(squares[index].prop("file")).toEqual(file);
+        })
       })
     })
   })

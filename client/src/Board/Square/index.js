296acae3a178d783d29fdcba26181777779b1335
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2019-01-01 23:10:04 -0500

    add alternating colors

--- client/src/Board/Square/index.js
+++ client/src/Board/Square/index.js
@@ -3,6 +3,17 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { withSize } from "react-sizeme";
 
+const files = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+];
+
 const Container = styled.div`
   height: 100px;
   border: 1px solid gray;
@@ -13,9 +24,37 @@ const Container = styled.div`
 `;
 
 function Square({ rank, file, size }) {
+  const fileIndexDivisibleBy2 = !(files.indexOf(file) % 2)
+  const rankDivisibleby2 = !(rank % 2)
+  let backgroundColor;
+  let color;
+
+
+  if (fileIndexDivisibleBy2 && !rankDivisibleby2) {
+    backgroundColor = "white"
+    color = "black";
+  }
+
+  if (!fileIndexDivisibleBy2 && !rankDivisibleby2) {
+    backgroundColor = "black"
+    color = "white";
+  }
+
+  if (!fileIndexDivisibleBy2 && rankDivisibleby2) {
+    backgroundColor = "white"
+    color = "black";
+  }
+
+  if (fileIndexDivisibleBy2 && rankDivisibleby2) {
+    backgroundColor = "black"
+    color = "white";
+  }
+
   return <Container
     style={{
-      height: size.width
+      height: size.width,
+      backgroundColor,
+      color
     }}>
     {file + rank}
   </Container>;

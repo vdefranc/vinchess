14b04c633b91f6bbb7e4b9e39509758d28d5e2fd
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2019-04-02 21:09:15 -0400

    delete cruft file

--- client/src/Board/Square/index.js
+++ client/src/Board/Square/index.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { withSize } from "react-sizeme";
 
-import SVGs from "./pieceSvgs";
 import getPieceSvg from "./vinPieceSvgs"
 
 const files = [
